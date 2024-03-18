703385075be34045c69009189d79ca7d3c5e0be9
eellison <elias.ellison@gmail.com>
2024-03-18 14:23:29 -0700

    Generate non-written to benchmarking inputs from existing memory instead of new allocation
    ghstack-source-id: 4dfa9f5c5f054f2092496800722f0c9ee20597c4
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/121999

--- torch/_inductor/select_algorithm.py
+++ torch/_inductor/select_algorithm.py
@@ -823,6 +823,8 @@ class AlgorithmSelectorCache(PersistentCache):
     ):
         from .codegen.cuda.cuda_kernel import CUDATemplateCaller
 
+        # TODO - assert that we have not mutating kernels here
+
         # TODO(nmacchioni): remove once CI tests are fixed
         choices = [choice for choice in choices if choice is not None]
 
@@ -1004,8 +1006,7 @@ class AlgorithmSelectorCache(PersistentCache):
             )
             for input_node in input_nodes
         ]
-
-        out = cls.benchmark_example_value(layout)
+        out = cls.benchmark_example_value(layout, written_to=True)
         out_extern = torch.as_strided(
             out, out.size(), out.stride(), V.graph.sizevars.size_hint(layout.offset)
         )
@@ -1139,7 +1140,7 @@ class AlgorithmSelectorCache(PersistentCache):
         sys.stderr.write(f"{autotune_type_str} AUTOTUNE takes {elapse:.4f} seconds ({precompile_elapse:.4f} precompile)\n")
 
     @staticmethod
-    def benchmark_example_value(node):
+    def benchmark_example_value(node, written_to=False):
         """
         Convert an ir.Buffer into a concrete torch.Tensor we can use for
         benchmarking.
@@ -1151,16 +1152,27 @@ class AlgorithmSelectorCache(PersistentCache):
             node = node.unwrap_view()
         # preserve rng states to avoid the rand_strided call below changes
         # the rng states for the real model code.
+        size = V.graph.sizevars.size_hints(
+            node.get_size(),
+            fallback=config.unbacked_symint_fallback,
+        )
+        stride = V.graph.sizevars.size_hints(
+            node.get_stride(),
+            fallback=config.unbacked_symint_fallback,
+        )
+        device = node.get_device()
+        dtype = node.get_dtype()
+        extra_size = node.layout.offset
+
+        if not written_to:
+            return torch._inductor.utils.get_read_only_benchmark_value(
+                size, stride, dtype=dtype, device=device, extra_size=extra_size
+            )
+
         with preserve_rng_state():
             return rand_strided(
-                V.graph.sizevars.size_hints(
-                    node.get_size(),
-                    fallback=config.unbacked_symint_fallback,
-                ),
-                V.graph.sizevars.size_hints(
-                    node.get_stride(),
-                    fallback=config.unbacked_symint_fallback,
-                ),
+                size,
+                stride,
                 device=node.get_device(),
                 dtype=node.get_dtype(),
                 extra_size=node.layout.offset,
